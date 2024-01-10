aaadeb64e0d06c92217ad11f86c5ec5afc496b4e
Abisheksivaraj <Abisheksivaraj2001@gmail.com>
2024-01-10 16:29:39 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Navbar from '../src/Components/Home/Navbar'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <Navbar/>
+  )
+
+}
+
+export default App
